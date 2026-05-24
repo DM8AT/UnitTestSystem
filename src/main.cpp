73240bb2fa110e8dbe40168f42eb8115705fd148
DM8AT <DM8AT@users.noreply.github.com>
2026-05-24 11:35:48 +0200

    added a readme

--- src/main.cpp
+++ src/main.cpp
@@ -25,31 +25,35 @@ int main() {
     if (testExecCreateExecutor(&execCreate, &executor) != TEST_EXEC_SUCCESS)
     {throw std::runtime_error("Failed to create an executor");}
 
-    //load a test
+    //load a test bundle
     TestExecLoadTestInfo loadInfo;
     loadInfo.path = "TestLib/Test1";
     if (testExecLoadTests(executor, &loadInfo) != TEST_EXEC_SUCCESS)
     {throw std::runtime_error("Failed to load the requested test");}
 
+    //get all tests loaded by the test bundle
     uint64_t testCount = 0;
     testExecEnumerateExecutorTests(executor, &testCount, nullptr);
     if (testCount == 0) {return 0;}
     std::vector<TestExecTest> tests(testCount);
     testExecEnumerateExecutorTests(executor, &testCount, tests.data());
 
+    //print the amount of discovered tests
     std::cout << testCount << " test(s) loaded" << "\n";
 
-    if (testCount == 0) {return 0;}
-
+    //select the first test to execute
     TestExecExecuteTestInfo execInfo;
     execInfo.priority = 0;
     execInfo.test = tests[0];
     testExecExecuteTest(executor, &execInfo);
 
+    //wait until the test was executed
     testExecWaitExecutorIdle(executor, UINT64_MAX);
 
+    //print the return value of the test
     TestExecTestResultInfo report;
     testExecGetTestResult(executor, tests[0], &report);
+    std::cout << "Test finished with exit code " << report.returnValue << "\n";
 
     //clean up
     testExecDestroyExecutor(executor);
